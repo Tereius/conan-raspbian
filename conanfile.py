b271b6577d7d03dc21694eea6c71081acba036ea
Björn Stresing <bjoern.stresing@gmx.de>
2020-12-18 23:17:01 +0100

    Replace absolute paths in libc.so with relative paths

--- conanfile.py
+++ conanfile.py
@@ -9,6 +9,7 @@ class RaspbianConan(ConanFile):
     name = "raspbian"
     version = "2020-02-14"
     description = "Linaro Cross-Toolchain"
+    license = "GPL"
     url = "https://github.com/Tereius/conan-raspbian"
     homepage = "https://github.com/Pro/raspi-toolchain"
     default_user = "tereius"
@@ -18,27 +19,31 @@ class RaspbianConan(ConanFile):
 
     def source(self):
         git = tools.Git("linux")
-        tools.download("https://raw.githubusercontent.com/Pro/raspi-toolchain/v1.0.1/Toolchain-rpi.cmake", "Toolchain-rpi.cmake")
+        tools.download("https://raw.githubusercontent.com/Pro/raspi-toolchain/v1.0.1/Toolchain-rpi.cmake",
+                       "Toolchain-rpi.cmake")
         tools.replace_in_file("Toolchain-rpi.cmake",
                               "set(CMAKE_PREFIX_PATH \"${CMAKE_PREFIX_PATH};${SYSROOT_PATH}/usr/lib/${TOOLCHAIN_HOST}\")",
                               "set(CMAKE_PREFIX_PATH \"${CMAKE_PREFIX_PATH};${SYSROOT_PATH}/usr/lib/${TOOLCHAIN_HOST}\")\nset(CMAKE_PREFIX_PATH \"${CMAKE_PREFIX_PATH};${SYSROOT_PATH}/opt/vc\")")
         tools.get("https://github.com/Pro/raspi-toolchain/releases/download/v1.0.1/raspi-toolchain.tar.gz")
-        
+
     @property
     def arch(self):
         if hasattr(self, "settings_target"):
             return self.settings_target.arch
         return self.settings.arch
-        
+
     @property
     def toolchainabi(self):
         return "arm-linux-gnueabihf"
-        
+
     def build(self):
         tools.unzip("rootfs.tar.gz", destination=self.package_folder)
 
     def package(self):
         self.copy(pattern="*")
+        tools.replace_in_file(
+            os.path.join(self.package_folder, "cross-pi-gcc", "arm-linux-gnueabihf", "lib", "libc.so"),
+            "/opt/cross-pi-gcc/arm-linux-gnueabihf/lib", ".")
 
     def define_tool_var(self, name, value, bin_folder):
         path = os.path.join(bin_folder, value)
@@ -61,10 +66,10 @@ class RaspbianConan(ConanFile):
 
         self.output.info('Appending PATH environment variable: %s' % toolchain_bin)
         self.env_info.PATH.append(toolchain_bin)
-        
+
         self.output.info('Creating CONAN_CMAKE_TOOLCHAIN_FILE environment variable: %s' % cmake_toolchain)
         self.env_info.CONAN_CMAKE_TOOLCHAIN_FILE = cmake_toolchain
-        
+
         rootfs = os.path.join(package, 'rootfs')
         self.output.info('Creating RASPBIAN_ROOTFS environment variable: %s' % rootfs)
         self.env_info.RASPBIAN_ROOTFS = rootfs
@@ -82,7 +87,7 @@ class RaspbianConan(ConanFile):
         self.env_info.OBJDUMP = self.define_tool_var('OBJDUMP', self.toolchainabi + '-objdump', toolchain_bin)
         self.env_info.READELF = self.define_tool_var('READELF', self.toolchainabi + '-readelf', toolchain_bin)
         self.env_info.ELFEDIT = self.define_tool_var('ELFEDIT', self.toolchainabi + '-elfedit', toolchain_bin)
-    
+
     def package_id(self):
         del self.info.settings.arch
         del self.info.settings.build_type
