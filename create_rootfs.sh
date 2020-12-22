6f3f6aac03e2bf26101c236769d337fc47b81b8f
Björn Stresing <bjoern.stresing@gmx.de>
2020-12-22 19:46:17 +0100

    Reduce the size of rootfs < 250 MB so we can upload it to jfrog

--- create_rootfs.sh
+++ create_rootfs.sh
@@ -4,6 +4,9 @@ current=$(pwd)
 
 rsync -vR --progress -rl --delete-after --copy-unsafe-links pi@$1:/{lib,usr,etc/ld.so.conf.d,opt/vc/lib,opt/vc/include} $(pwd)/rootfs
 
+rm -r $(pwd)/rootfs/usr/bin/*
+rm -r $(pwd)/rootfs/usr/sbin/*
+rm -r $(pwd)/rootfs/usr/share/*
 mv $(pwd)/rootfs/usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0 $(pwd)/rootfs/usr/lib/arm-linux-gnueabihf/libEGL.so.1.0.0_backup
 mv $(pwd)/rootfs/usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0 $(pwd)/rootfs/usr/lib/arm-linux-gnueabihf/libGLESv2.so.2.0.0_backup
 cd $(pwd)/rootfs/opt/vc/lib
